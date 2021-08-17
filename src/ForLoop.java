bffa6ec81bb5c9b391fb75fe512c2ffc014a44d1
CristiDablea <cristidablea@yahoo.com>
2021-08-17 23:09:32 +0300

    Adaug un exercitiu for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        // for loop = executa un bloc de cod de un numar limitat de ori
+
+        for(int i=10; i>=0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("An nou fericit!");
+    }
+
+}
